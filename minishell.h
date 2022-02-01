b003dfc59a8f5c04aeeaebf3d9056b9a37e96a00
osni junior <osniandrade@gmail.com>
2022-02-01 21:03:42 +0000

    added struct to header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 23:16:23 by ocarlos-          #+#    #+#             */
-/*   Updated: 2022/01/18 23:20:41 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2022/01/20 23:15:05 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,14 @@ typedef struct s_data
 	char	**args;
 	char	*cmd;
 }			t_data;
+
+typedef struct s_hashlist
+{
+	struct hashlist	*next;
+	char			*command;
+	char			*fullpath;
+}					t_hashlist;
+
 
 // helper1.c
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
